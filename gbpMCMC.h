11a8cb30022bc64001a87e61c605ae489f6ee6f4
Gregory Poole <gbpoole@gmail.com>
2011-01-06 14:41:33 +1100

    Added a function (set_MCMC_likelihood_function) so that the user can define their own likelihood function.

--- gbpMCMC.h
+++ gbpMCMC.h
@@ -71,6 +71,7 @@ void compute_MCMC(MCMC_info *MCMC);
 void set_MCMC_temperature(MCMC_info *MCMC,double temperature);
 void set_MCMC_iterations(MCMC_info *MCMC,int n_avg,int n_avg_covariance,int n_thin,int n_burn,int n_integrate);
 void set_MCMC_coverage_size(MCMC_info *MCMC,int coverage_size);
+void set_MCMC_likeliood_function(MCMC_info *MCMC,void (*likelihood_function)(MCMC_info *,double **,double *));
 void set_MCMC_directory(MCMC_info *MCMC,char *directory);
 void set_MCMC_mode(MCMC_info *MCMC,int mode);
 
---
+++ set_MCMC_likelihood_function.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
+#include <gbpLib.h>
+#include <gbpRNG.h>
+#include <gbpMCMC.h>
+
+void set_MCMC_likeliood_function(MCMC_info *MCMC,void (*likelihood_function)(MCMC_info *,double **,double *)){
+  MCMC->compute_MCMC_ln_likelihood=likelihood_function;
+}
+
