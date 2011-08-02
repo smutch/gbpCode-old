2eb63260eaa0ad15e14f7aef135ac8928951e197
Gregory Poole <gbpoole@gmail.com>
2011-08-02 14:09:08 +0200

    Some weirdness due to a crash during a conflict resolution.

--- gbpMath/gbpStats/gbpStats.h.REMOTE.4426.h
+++ gbpMath/gbpStats/gbpStats.h.REMOTE.4426.h
@@ -13,11 +13,4 @@ void calc_median_global(void   *data_local,
                         SID_Datatype type,
                         int          mode,
                         SID_Comm    *comm);
-double calc_sep_periodic(double x_1,
-                         double y_1,
-                         double z_1,
-                         double x_2,
-                         double y_2,
-                         double z_2,
-                         double box_size);
 #endif
