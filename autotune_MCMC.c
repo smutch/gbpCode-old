0bf0acd421c07d8a9cc1f13776bd45291861ef4e
Gregory Poole <gbpoole@gmail.com>
2012-01-15 23:43:27 +1100

    Changed autotuning so that autotune_MCMC_randomize is ignored if n_autotune_randomize<=0.

--- autotune_MCMC.c
+++ autotune_MCMC.c
@@ -8,7 +8,8 @@
 void autotune_MCMC(MCMC_info *MCMC){
   int i_tune;
   SID_log("Perform autotuning...",SID_LOG_OPEN|SID_LOG_TIMER);
-  autotune_MCMC_randomize(MCMC);
+  if(MCMC->n_autotune_randomize>0)
+    autotune_MCMC_randomize(MCMC);
   for(i_tune=0;i_tune<MCMC->n_autotune;i_tune++){
     SID_log("Autotune iteration %d of %d...",SID_LOG_OPEN|SID_LOG_TIMER,i_tune+1,MCMC->n_autotune);
     autotune_MCMC_temperature(MCMC);
