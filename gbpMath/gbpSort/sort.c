c1fc3d03f45a137a0cf4fed844f693f46265b2da
Gregory Poole <gbpoole@gmail.com>
2013-05-30 20:57:10 +1000

    Substantial improvements to parallel sorting.  Faster and less RAM use.

--- gbpMath/gbpSort/sort.c
+++ gbpMath/gbpSort/sort.c
@@ -18,11 +18,10 @@ void sort(void          *sval,
   int     rank_send_to;
   int     rank_receive_from;
   size_t  i,ir,j,l,k;
-  size_t  nval_max;
   size_t  nval_tmp;
   size_t  nval_all;
   size_t *index_tmp;
-  size_t *increment;
+  size_t *rank;
   int     data_type_size_i;
   size_t  data_type_size;
   void   *sval_tmp;
@@ -55,6 +54,7 @@ void sort(void          *sval,
   }
   //   ... perform a global search returning ranks
   else{
+    #if USE_MPI
     // Rank local array in ascending order; must
     //   use merge_sort (despite extra RAM use) 
     //   because it is "stable"
@@ -63,101 +63,261 @@ void sort(void          *sval,
 	       nval,
 	       index,
 	       data_type,
-	       SORT_COMPUTE_RANK,
+	       SORT_COMPUTE_INDEX,
 	       SORT_COMPUTE_NOT_INPLACE);
-    SID_log("Done.",SID_LOG_CLOSE);
 
     // If this is being run in parallel, consider
     //   the arrays on the other ranks as well
     //   if flag_local is set to SORT_GLOBAL
-    #if USE_MPI
+
+    // ... create a local array to hold results for the local rank ...
+    size_t *rank=NULL;
+
+    // ... initialize it with the local rank values ...
+    merge_sort(sval,
+	       nval,
+	       &rank,
+	       data_type,
+	       SORT_COMPUTE_RANK,
+	       SORT_COMPUTE_NOT_INPLACE);
+    SID_log("Done.",SID_LOG_CLOSE);
+
+    // ... get the largest number of items on any rank ...
+    SID_Allreduce(&nval,&nval_tmp,1,SID_SIZE_T,SID_MAX,SID.COMM_WORLD);
+
+    // ... get the size of our data-type ...
+    SID_Type_size(data_type,&data_type_size_i);
+    data_type_size=(size_t)data_type_size_i;
+
+    // Process the items on other ranks
     if(flag_local==SORT_GLOBAL && SID.n_proc>1){
       SID_log("Sorting distributed items...",SID_LOG_OPEN|SID_LOG_TIMER);
 
-      // ... get the largest number of items on any rank ...
-      SID_Allreduce(&nval,&nval_max,1,SID_SIZE_T,SID_MAX,SID.COMM_WORLD);
-
-      // ... get the size of our data-type ...
-      SID_Type_size(data_type,&data_type_size_i);
-      data_type_size=(size_t)data_type_size_i;
-
-      // ... create two temporary arrays that are needed for the sort ...
-      sval_tmp =(void   *)SID_malloc((size_t)data_type_size*(nval+nval_max));
-      increment=(size_t *)SID_calloc(sizeof(size_t)*nval);
+      // ... create two temporary arrays for exchanges ...
+      sval_tmp =(void   *)SID_malloc((size_t)data_type_size*nval_tmp);
+      index_tmp=(size_t *)SID_malloc(sizeof(size_t)*nval_tmp);
 
       // ... perform exchanges and sorts ...
       for(i_rank=1;i_rank<SID.n_proc;i_rank++){
         SID_log("Rank exchange %d of %d...",SID_LOG_OPEN|SID_LOG_TIMER,i_rank,SID.n_proc-1);
 
-	// Ranks < My_rank are placed first in the new data list
-	//  so that duplicate entries are placed in a unique order
+	// Perform a ring exchange of all data and their local sort indices
         set_exchange_ring_ranks(&rank_send_to,&rank_receive_from,i_rank);
-        if(rank_receive_from<SID.My_rank){
-           exchange_ring_buffer(sval,
-                                data_type_size,
-                                nval,
-                                sval_tmp,
-                                &nval_tmp,
-                                i_rank);
-           memcpy(&(((char *)sval_tmp)[nval_tmp*data_type_size]),sval,nval*data_type_size);
-        }
-        else{
-           exchange_ring_buffer(sval,
-                                data_type_size,
-                                nval,
-                                &(((char *)sval_tmp)[nval*data_type_size]),
-                                &nval_tmp,
-                                i_rank);
-           memcpy(sval_tmp,sval,nval*data_type_size);
-        }
-        nval_all=nval+nval_tmp;
+        exchange_ring_buffer(sval,
+                             data_type_size,
+                             nval,
+                             sval_tmp,
+                             &nval_tmp,
+                             i_rank);
+        exchange_ring_buffer((*index),
+                             sizeof(size_t),
+                             nval,
+                             index_tmp,
+                             &nval_tmp,
+                             i_rank);
 
-        if(nval_all>0){
-           // A STABLE sort algorythm MUST be used here!
-	   merge_sort(sval_tmp,
-	   	   nval_all,
-	   	   &index_tmp,
-	   	   data_type,
-	   	   SORT_COMPUTE_RANK,
-	   	   SORT_COMPUTE_INPLACE);
-
-	   // Figure-out where the local values lie in the new sort
-	   //   and increment rank for each if any values from
-	   //   receive_from_rank's array are less than it
+        // Increment the local sort rank by dispplacements computed from other ranks
+        if(data_type==SID_DOUBLE){
+           double *sval_dtype    =(double *)sval;
+           double *sval_tmp_dtype=(double *)sval_tmp;
 	   if(rank_receive_from<SID.My_rank){
-	     for(i=0,j=0;i<nval;i++)
-	       increment[i]+=index_tmp[i+nval_tmp]-(*index)[i];
+	     for(i=0,j=0;i<nval;i++){
+               if(j<nval_tmp){
+                  // Equal values from lower ranks come before (hence '>=')
+                  while(sval_dtype[(*index)[i]]>=sval_tmp_dtype[index_tmp[j]]){
+                    j++;
+                    if(j==nval_tmp)
+                      break;
+                  }
+               }
+               rank[(*index)[i]]+=j;
+             }
 	   }
 	   else{
-	     for(i=0,j=0;i<nval;i++)
-	       increment[i]+=index_tmp[i]-(*index)[i];
+	     for(i=0,j=0;i<nval;i++){
+               if(j<nval_tmp){
+                  // Equal values from larger ranks come after (hence '>')
+                  while(sval_dtype[(*index)[i]]>sval_tmp_dtype[index_tmp[j]]){
+                    j++;
+                    if(j==nval_tmp)
+                      break;
+                  }
+               }
+               rank[(*index)[i]]+=j;
+             }
 	   }
-
-           // Free sort ranks
-	   SID_free(SID_FARG index_tmp);
         }
+        else if(data_type==SID_FLOAT){
+           float *sval_dtype    =(float *)sval;
+           float *sval_tmp_dtype=(float *)sval_tmp;
+	   if(rank_receive_from<SID.My_rank){
+	     for(i=0,j=0;i<nval;i++){
+               if(j<nval_tmp){
+                  // Equal values from lower ranks come before (hence '>=')
+                  while(sval_dtype[(*index)[i]]>=sval_tmp_dtype[index_tmp[j]]){
+                    j++;
+                    if(j==nval_tmp)
+                      break;
+                  }
+               }
+               rank[(*index)[i]]+=j;
+             }
+	   }
+	   else{
+	     for(i=0,j=0;i<nval;i++){
+               if(j<nval_tmp){
+                  // Equal values from larger ranks come after (hence '>')
+                  while(sval_dtype[(*index)[i]]>sval_tmp_dtype[index_tmp[j]]){
+                    j++;
+                    if(j==nval_tmp)
+                      break;
+                  }
+               }
+               rank[(*index)[i]]+=j;
+             }
+	   }
+        }
+        else if(data_type==SID_INT){
+           int *sval_dtype    =(int *)sval;
+           int *sval_tmp_dtype=(int *)sval_tmp;
+	   if(rank_receive_from<SID.My_rank){
+	     for(i=0,j=0;i<nval;i++){
+               if(j<nval_tmp){
+                  // Equal values from lower ranks come before (hence '>=')
+                  while(sval_dtype[(*index)[i]]>=sval_tmp_dtype[index_tmp[j]]){
+                    j++;
+                    if(j==nval_tmp)
+                      break;
+                  }
+               }
+               rank[(*index)[i]]+=j;
+             }
+	   }
+	   else{
+	     for(i=0,j=0;i<nval;i++){
+               if(j<nval_tmp){
+                  // Equal values from larger ranks come after (hence '>')
+                  while(sval_dtype[(*index)[i]]>sval_tmp_dtype[index_tmp[j]]){
+                    j++;
+                    if(j==nval_tmp)
+                      break;
+                  }
+               }
+               rank[(*index)[i]]+=j;
+             }
+	   }
+        }
+        else if(data_type==SID_UNSIGNED){
+           unsigned int *sval_dtype    =(unsigned int *)sval;
+           unsigned int *sval_tmp_dtype=(unsigned int *)sval_tmp;
+	   if(rank_receive_from<SID.My_rank){
+	     for(i=0,j=0;i<nval;i++){
+               if(j<nval_tmp){
+                  // Equal values from lower ranks come before (hence '>=')
+                  while(sval_dtype[(*index)[i]]>=sval_tmp_dtype[index_tmp[j]]){
+                    j++;
+                    if(j==nval_tmp)
+                      break;
+                  }
+               }
+               rank[(*index)[i]]+=j;
+             }
+	   }
+	   else{
+	     for(i=0,j=0;i<nval;i++){
+               if(j<nval_tmp){
+                  // Equal values from larger ranks come after (hence '>')
+                  while(sval_dtype[(*index)[i]]>sval_tmp_dtype[index_tmp[j]]){
+                    j++;
+                    if(j==nval_tmp)
+                      break;
+                  }
+               }
+               rank[(*index)[i]]+=j;
+             }
+	   }
+        }
+        else if(data_type==SID_SIZE_T){
+           size_t *sval_dtype    =(size_t *)sval;
+           size_t *sval_tmp_dtype=(size_t *)sval_tmp;
+	   if(rank_receive_from<SID.My_rank){
+	     for(i=0,j=0;i<nval;i++){
+               if(j<nval_tmp){
+                  // Equal values from lower ranks come before (hence '>=')
+                  while(sval_dtype[(*index)[i]]>=sval_tmp_dtype[index_tmp[j]]){
+                    j++;
+                    if(j==nval_tmp)
+                      break;
+                  }
+               }
+               rank[(*index)[i]]+=j;
+             }
+	   }
+	   else{
+	     for(i=0,j=0;i<nval;i++){
+               if(j<nval_tmp){
+                  // Equal values from larger ranks come after (hence '>')
+                  while(sval_dtype[(*index)[i]]>sval_tmp_dtype[index_tmp[j]]){
+                    j++;
+                    if(j==nval_tmp)
+                      break;
+                  }
+               }
+               rank[(*index)[i]]+=j;
+             }
+	   }
+        }
+        else if(data_type==SID_LONG_LONG){
+           long long *sval_dtype    =(long long *)sval;
+           long long *sval_tmp_dtype=(long long *)sval_tmp;
+	   if(rank_receive_from<SID.My_rank){
+	     for(i=0,j=0;i<nval;i++){
+               if(j<nval_tmp){
+                  // Equal values from lower ranks come before (hence '>=')
+                  while(sval_dtype[(*index)[i]]>=sval_tmp_dtype[index_tmp[j]]){
+                    j++;
+                    if(j==nval_tmp)
+                      break;
+                  }
+               }
+               rank[(*index)[i]]+=j;
+             }
+	   }
+	   else{
+	     for(i=0,j=0;i<nval;i++){
+               if(j<nval_tmp){
+                  // Equal values from larger ranks come after (hence '>')
+                  while(sval_dtype[(*index)[i]]>sval_tmp_dtype[index_tmp[j]]){
+                    j++;
+                    if(j==nval_tmp)
+                      break;
+                  }
+               }
+               rank[(*index)[i]]+=j;
+             }
+	   }
+        }
+        else
+          SID_trap_error("Unknown variable type in sort().",ERROR_LOGIC);
+
         SID_log("Done.",SID_LOG_CLOSE);
       } // loop over ranks
 
-      for(i=0;i<nval;i++)
-	(*index)[i]+=increment[i];
-    
-      SID_free(SID_FARG increment);
       SID_free(SID_FARG sval_tmp);
+      SID_free(SID_FARG index_tmp);
       SID_log("Done.",SID_LOG_CLOSE);
     } // if rank exchange is needed
+     
+    // We want global ranks to be stored in 'index' at this point
+    SID_free(SID_FARG (*index));
+    (*index)=rank;
 
+    // If we want a global index, then we need to sort the global ranks stored in 'index'
     if(flag_local        ==SORT_GLOBAL && 
        flag_compute_index!=SORT_COMPUTE_RANK){
       size_t *sort_ranks;
       SID_log("Generating sort indices from sort ranks...",SID_LOG_OPEN|SID_LOG_TIMER);
-      // ... get the largest number of items on any rank ...
-      SID_Allreduce(&nval,&nval_max,1,SID_SIZE_T,SID_MAX,SID.COMM_WORLD);
-      // ... get the size of our data-type ...
-      SID_Type_size(data_type,&data_type_size_i);
-      data_type_size=(size_t)data_type_size_i;
-      // ... determine what the first element of the sort indices
-      //     is for each rank ..
+      // ... determine what the first array index is for each rank ..
       size_t first_index;
       for(i_rank=0,first_index=0;i_rank<SID.n_proc;i_rank++){
         nval_tmp=nval;
@@ -171,8 +331,8 @@ void sort(void          *sval,
       size_t  offset;
       size_t  i_val;
       sort_ranks=(*index);
-      (*index)  =(size_t *)SID_malloc(sizeof(size_t)*nval);
-      rank_rank =(size_t *)SID_malloc(sizeof(size_t)*nval_max);
+      (*index)  =(size_t *)SID_calloc(sizeof(size_t)*nval);
+      rank_rank =(size_t *)SID_malloc(sizeof(size_t)*nval_tmp);
       // ... loop over ranks, performing exchanges and setting indices ...
       for(i_rank=0,offset=0;i_rank<SID.n_proc;i_rank++){
         nval_tmp=nval;
@@ -188,12 +348,14 @@ void sort(void          *sval,
         }
         offset+=nval_tmp;
       }
-
       SID_free(SID_FARG sort_ranks);
       SID_free(SID_FARG rank_rank);
       SID_log("Done.",SID_LOG_CLOSE);
     }
+    #else
+      SID_trap_error("Undefined behavior in sort().",ERROR_LOGIC);
     #endif
   }
   SID_log("Done.",SID_LOG_CLOSE);
 }
+
