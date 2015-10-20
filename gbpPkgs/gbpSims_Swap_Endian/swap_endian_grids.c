b437e6a6c19923f12a47ac84c22b1a4c63f7122a
Gregory Poole <gbpoole@gmail.com>
2015-10-21 10:20:29 +1100

    Changed the swap_endian code for grids to not fail if the file isn't present.

--- gbpPkgs/gbpSims_Swap_Endian/swap_endian_grids.c
+++ gbpPkgs/gbpSims_Swap_Endian/swap_endian_grids.c
@@ -19,49 +19,51 @@ void swap_endian_grids(const char *filename_in,const char *filename_out,int mode
   FILE *fp_in =NULL;
   FILE *fp_out=NULL;
   if((fp_in=fopen(filename_in,"r"))==NULL)
-     SID_trap_error("Could not open {%s} for reading.",ERROR_IO_OPEN,filename_in);
-  if((fp_out=fopen(filename_out,"w"))==NULL)
-     SID_trap_error("Could not open {%s} for writing.",ERROR_IO_OPEN,filename_out);
+     SID_log("not present.",SID_LOG_CLOSE,filename_in);
+  else{
+     if((fp_out=fopen(filename_out,"w"))==NULL)
+        SID_trap_error("Could not open {%s} for writing.",ERROR_IO_OPEN,filename_out);
 
-  // Read the needed header information and rewind
-  int    n[3];
-  double L[3];
-  int    n_grids;
-  int    scheme;
-  fread(&(n[0]), sizeof(int),   1,fp_in);
-  fread(&(n[1]), sizeof(int),   1,fp_in);
-  fread(&(n[2]), sizeof(int),   1,fp_in);
-  fread(&(L[0]), sizeof(double),1,fp_in);
-  fread(&(L[1]), sizeof(double),1,fp_in);
-  fread(&(L[2]), sizeof(double),1,fp_in);
-  fread(&n_grids,sizeof(int),   1,fp_in);
-  fread(&scheme, sizeof(int),   1,fp_in);
-  if(check_mode_for_flag(mode,SWAP_SSIMPL_ENDIAN_TO_NATIVE)){
-     swap_endian((char *)(n),       3,sizeof(int));
-     swap_endian((char *)(&n_grids),1,sizeof(int));
-  }
-  int grid_size=n[0]*n[1]*n[2];
-  rewind(fp_in);
+     // Read the needed header information and rewind
+     int    n[3];
+     double L[3];
+     int    n_grids;
+     int    scheme;
+     fread(&(n[0]), sizeof(int),   1,fp_in);
+     fread(&(n[1]), sizeof(int),   1,fp_in);
+     fread(&(n[2]), sizeof(int),   1,fp_in);
+     fread(&(L[0]), sizeof(double),1,fp_in);
+     fread(&(L[1]), sizeof(double),1,fp_in);
+     fread(&(L[2]), sizeof(double),1,fp_in);
+     fread(&n_grids,sizeof(int),   1,fp_in);
+     fread(&scheme, sizeof(int),   1,fp_in);
+     if(check_mode_for_flag(mode,SWAP_SSIMPL_ENDIAN_TO_NATIVE)){
+        swap_endian((char *)(n),       3,sizeof(int));
+        swap_endian((char *)(&n_grids),1,sizeof(int));
+     }
+     int grid_size=n[0]*n[1]*n[2];
+     rewind(fp_in);
 
-  // Create a read buffer
-  char *buffer=(char *)SID_malloc(sizeof(char)*grid_size*sizeof(fftw_real));
+     // Create a read buffer
+     char *buffer=(char *)SID_malloc(sizeof(char)*grid_size*sizeof(fftw_real));
 
-  // Process the file
-  rewrite_swap_endian(fp_in,fp_out,3,sizeof(int),   buffer);
-  rewrite_swap_endian(fp_in,fp_out,3,sizeof(double),buffer);
-  rewrite_swap_endian(fp_in,fp_out,2,sizeof(int),   buffer);
-  for(int i_grid=0;i_grid<n_grids;i_grid++){
-     rewrite_swap_endian(fp_in,fp_out,GRID_IDENTIFIER_SIZE,sizeof(char),     buffer);
-     rewrite_swap_endian(fp_in,fp_out,grid_size,           sizeof(fftw_real),buffer);
-  }
+     // Process the file
+     rewrite_swap_endian(fp_in,fp_out,3,sizeof(int),   buffer);
+     rewrite_swap_endian(fp_in,fp_out,3,sizeof(double),buffer);
+     rewrite_swap_endian(fp_in,fp_out,2,sizeof(int),   buffer);
+     for(int i_grid=0;i_grid<n_grids;i_grid++){
+        rewrite_swap_endian(fp_in,fp_out,GRID_IDENTIFIER_SIZE,sizeof(char),     buffer);
+        rewrite_swap_endian(fp_in,fp_out,grid_size,           sizeof(fftw_real),buffer);
+     }
 
-  // Free the read buffer
-  SID_free(SID_FARG buffer);
+     // Free the read buffer
+     SID_free(SID_FARG buffer);
 
-  // Close files
-  fclose(fp_in);
-  fclose(fp_out);
+     // Close files
+     fclose(fp_in);
+     fclose(fp_out);
   
-  SID_log("Done.",SID_LOG_CLOSE);
+     SID_log("Done.",SID_LOG_CLOSE);
+  }
 }
 
