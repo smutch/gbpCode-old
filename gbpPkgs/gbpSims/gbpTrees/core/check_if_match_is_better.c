8a9a9c21fe23768c4c36da3a58f64085b07477cc
Gregory Poole <gbpoole@gmail.com>
2016-05-04 23:35:02 +1000

    Additional tweeks of projenitor building in gbpTrees, partially undoing last commit's changes.

--- gbpPkgs/gbpSims/gbpTrees/core/check_if_match_is_better.c
+++ gbpPkgs/gbpSims/gbpTrees/core/check_if_match_is_better.c
@@ -30,32 +30,26 @@ int check_if_match_is_better(tree_horizontal_info *target_halo,
                flag_valid=FALSE;
             // ... otherwise, select via secondary criteria ...
             else if(file_offset_new==file_offset_old){
-               // The following XXX'd-out scheme fails when snapshot densities are high.  In these cases, small
-               //   halo's passing transiently through cores spend several snapshots in this state, creating
-               //   2-way matches and leading to the problem this choice was meant to correct.  For this reason,
-               //   we now simply take the match with the largest score.
-               // XXX // With f_good>0, the following scheme was found to create lots of poor matches due to small
-               // XXX //   halos getting matched to/by the centrals of large systems, leading to lots of spurious
-               // XXX //   and very large fragmented halos, particularly when snapshot cadence becomes dense.  For
-               // XXX //   that reason, f_good must be kept to zero so that all possible back matches created by 
-               // XXX //   FoF core swaps are considered.
-               // XXX // If equally immediate, give first preference to 2-way matches ...
-               // XXX if(old_match->flag_two_way)
-               // XXX    flag_valid=FALSE;
-               // XXX // ... otherwise, give secondary preference to the halo with the best metrics.
-               // XXX //     Unfortunately, the best choice of metric(s) is not perfectly obvious:
-               // XXX //    ... using match score for this decision introduces the problem
-               // XXX //        of sometimes selecting a small substructure that sinks quickly to (or
-               // XXX //        passes transiently through) the middle of a larger halo.  This 
-               // XXX //        problem is reduced by giving preference to 2-way matches (above).
-               // XXX //    ... using n_particles for this decision can be compromised
-               // XXX //        by ambiguities introduced by the large halo size fluctuations
-               // XXX //        introduced by transient exchanges of outer halo particles or
-               // XXX //        (equivilantly) by changes in the identity of the substructure
-               // XXX //        core the halo finder reports as the centre of the system
-               // XXX else if(!new_match->flag_two_way && (old_match->score>new_match->score))
-               // XXX    flag_valid=FALSE;
-               if(old_match->score>new_match->score)
+               // With f_good>0, the following scheme was found to create lots of poor matches due to small
+               //   halos getting matched to/by the centrals of large systems, leading to lots of spurious
+               //   and very large fragmented halos, particularly when snapshot cadence becomes dense.  For
+               //   that reason, f_good must be kept to zero so that all possible back matches created by 
+               //   FoF core swaps are considered.
+               // If equally immediate, give first preference to 2-way matches ...
+               if(old_match->flag_two_way)
+                  flag_valid=FALSE;
+               // ... otherwise, give secondary preference to the halo with the best metrics.
+               //     Unfortunately, the best choice of metric(s) is not perfectly obvious:
+               //    ... using match score for this decision introduces the problem
+               //        of sometimes selecting a small substructure that sinks quickly to (or
+               //        passes transiently through) the middle of a larger halo.  This 
+               //        problem is reduced by giving preference to 2-way matches (above).
+               //    ... using n_particles for this decision can be compromised
+               //        by ambiguities introduced by the large halo size fluctuations
+               //        introduced by transient exchanges of outer halo particles or
+               //        (equivilantly) by changes in the identity of the substructure
+               //        core the halo finder reports as the centre of the system
+               else if(!new_match->flag_two_way && (old_match->score>new_match->score))
                   flag_valid=FALSE;
             }
          }
