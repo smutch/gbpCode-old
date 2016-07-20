4b89c2110196986165c54e943abea56c48bd6ec9
Gregory Poole <gbpoole@gmail.com>
2016-07-20 16:22:31 +1000

    A fix to the recursive function that computes inclusive particle counts.

--- gbpPkgs/gbpSims/gbpTrees/core/compute_inclusive_particle_count_recursive.c
+++ gbpPkgs/gbpSims/gbpTrees/core/compute_inclusive_particle_count_recursive.c
@@ -11,17 +11,22 @@
 
 void compute_inclusive_particle_count_recursive(tree_info *trees,tree_node_info *this_halo,int *n_particles_substructure){
   // Loop over each substructure (recursively)
-  int n_particles_substructure_sum=0;
+  int n_particles_substructure_sum    =0;
   tree_node_info *first_substructure  =this_halo->substructure_first;
   tree_node_info *current_substructure=first_substructure;
   while(current_substructure!=NULL){
      int n_particles_substructure_i;
      compute_inclusive_particle_count_recursive(trees,current_substructure,&n_particles_substructure_i);
      n_particles_substructure_sum+=n_particles_substructure_i;
-     current_substructure=current_substructure->substructure_next;
+     current_substructure         =current_substructure->substructure_next;
   }
-  this_halo->n_particles_inclusive+=n_particles_substructure_sum;
-  if(n_particles_substructure!=NULL)
+  // If this pointer is NULL, then it means that
+  //    this function has been passed a group and
+  //    we don't want to alter it's particle count,
+  //    which is already inclusive by construction
+  if(n_particles_substructure!=NULL){
+     this_halo->n_particles_inclusive+=n_particles_substructure_sum;
      (*n_particles_substructure)=this_halo->n_particles_inclusive;
+  }
 }
 
